71b813047e99a00f6c0f696b8d1766d533ba6cc6
carsonblack <carsonblack@gmail.com>
2020-05-14 12:20:15 -0700

    Adding a coming soon landing page for the index.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const countdown = document.querySelector('.countdown');
+
+// Set launch date in milliseconds.
+const launchDate = new Date('May 18, 2020 00:00:01').getTime();
+
+//Update counter every second.
+const intvl = setInterval(() => {
+  // Get today's date and time in milliseconds.
+  const now = new Date().getTime();
+
+  // Distance from now to launch dateaxios.all([callback1,callback2])
+  const distance = launchDate - now;
+
+  // Time calculations
+  const days = Math.floor(distance / (1000 * 60 * 60 *24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display result
+  countdown.innerHTML = `
+    <div class="bordered">${days} <span>Days</span></div>
+    <div class="bordered">${hours} <span>Hours</span></div>
+    <div class="bordered">${minutes} <span>Minutes</span></div>
+    <div class="bordered">${seconds} <span>Seconds</span></div>
+  `;
+
+  // If the date is passed stop countdown
+  if (distance < 0) {
+    clearInterval(intvl);
+    //Style output and set Launched text
+    countdown.style.color = '#038542';
+    countdown.innerHTML = 'Launched!';
+  }
+}, 1000);
+
+
