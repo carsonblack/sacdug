323a6dbaa2ecdf6893b4728205357e4cea1320c6
carsonblack <carsonblack@gmail.com>
2020-05-27 20:49:38 -0700

    Turning Countdown into a Vue Component.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-const countdown = document.querySelector('.countdown');
 
 // Set launch date in milliseconds.
-const launchDate = new Date('May 23, 2020 24:00:00').getTime();
+const launchDate = new Date('June 23, 2020 24:00:00').getTime();
 
 //Update counter every second.
 const intvl = setInterval(() => {
@@ -11,27 +10,11 @@ const intvl = setInterval(() => {
   // Distance from now to launch dateaxios.all([callback1,callback2])
   const distance = launchDate - now;
 
-  // Time calculations
-  const days = Math.floor(distance / (1000 * 60 * 60 *24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display result
-  countdown.innerHTML = `
-    <div class="bordered">${days} <span>Days</span></div>
-    <div class="bordered">${hours} <span>Hours</span></div>
-    <div class="bordered">${minutes} <span>Minutes</span></div>
-    <div class="bordered">${seconds} <span>Seconds</span></div>
-  `;
-
   // If the date is passed stop countdown
   if (distance < 0) {
     clearInterval(intvl);
-    //Style output and set Launched text
-    countdown.style.color = '#038542';
-    countdown.innerHTML = 'Launched!';
   }
+  return distance;
 }, 1000);
 
 // Takes date object with date and time set and formats the time to something
@@ -46,6 +29,28 @@ const formatTime = (dateString) => {
   return date.toLocaleTimeString('en-US', timeOptions);
 };
 
+Vue.component('countdown', {
+  template: `<div class="countdown">
+    <div v-if="launched" class="launched">Launched!</div>
+    <div v-else>
+      <div class="bordered">{{ days }} <span>Days</span></div>
+      <div class="bordered">{{ hours }} <span>Hours</span></div>
+      <div class="bordered">{{ minutes }} <span>Minutes</span></div>
+      <div class="bordered">{{ seconds }} <span>Seconds</span></div>
+    </div>
+  </div>`,
+  data() {
+    return {
+      launched: intvl <= 0 ? true : false,
+      days: Math.floor(intvl / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((intvl % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((intvl % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((intvl % (1000 * 60)) / 1000),
+    }
+  }
+})
+
+new Vue({ el: '.landing-inner' })
 
 Vue.component('nextevent', {
   template: `
@@ -72,5 +77,3 @@ Vue.component('nextevent', {
 })
 
 new Vue({ el: '#eventDisplay' })
-
-
